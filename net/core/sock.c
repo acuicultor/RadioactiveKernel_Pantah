fabdb2c6e33cb8a171baa668a52ca0761520b948
acuicultor <ramoncuenca4@gmail.com>
2023-01-25 09:57:42 +0100

    Revert "tcp: add RETPOLINE mitigation to sk_backlog_rcv"
    This reverts commit c4f8742a47c8e021600dda541f1c05f6d8d7a67c.

--- net/core/sock.c
+++ net/core/sock.c
@@ -326,10 +326,7 @@ int __sk_backlog_rcv(struct sock *sk, struct sk_buff *skb)
 	BUG_ON(!sock_flag(sk, SOCK_MEMALLOC));
 
 	noreclaim_flag = memalloc_noreclaim_save();
-	ret = INDIRECT_CALL_INET(sk->sk_backlog_rcv,
-				 tcp_v6_do_rcv,
-				 tcp_v4_do_rcv,
-				 sk, skb);
+	ret = sk->sk_backlog_rcv(sk, skb);
 	memalloc_noreclaim_restore(noreclaim_flag);
 
 	return ret;
