855dfaec72a2331c696bc9720a2074f806437bc4
acuicultor <ramoncuenca4@gmail.com>
2023-04-30 19:26:45 +0200

    treewide: fix errors caught by clang
    Signed-off-by: acuicultor <ramoncuenca4@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -4073,19 +4073,17 @@ static void lru_gen_age_node(struct pglist_data *pgdat, struct scan_control *sc)
 		return;
 
 	/*
-	 * The main goal is to OOM kill if every generation from all memcgs is
-	 * younger than min_ttl. However, another possibility is all memcgs are
-	 * either below min or empty.
-	 */
-	if (mutex_trylock(&oom_lock)) {
-		struct oom_control oc = {
-			.gfp_mask = sc->gfp_mask,
-		};
-
-		out_of_memory(&oc);
-
-		mutex_unlock(&oom_lock);
-	}
+     * The main goal is to OOM kill if every generation from all memcgs is
+     * younger than min_ttl. However, another possibility is all memcgs are
+     * either below min or empty.
+     */
+    if (mutex_trylock(&oom_lock)) {
+    	struct oom_control oc = {
+		   .gfp_mask = sc->gfp_mask,
+	    };
+	    out_of_memory(&oc);
+	    mutex_unlock(&oom_lock);
+    }
 }
 
 /*
