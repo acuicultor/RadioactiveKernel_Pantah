ab5ad1da8fbc1188913f799ae8f46f6aa364dd4d
Pzqqt <821026875@qq.com>
2023-07-12 23:36:58 +0400

    fs: Add KernelSU calls manually
    - Guide: https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    - Wrap them with `if` macros.
    - These modifications are required when `CONFIG_KPROBES` is not enabled.
    - This fixes compile errors when `CONFIG_KSU` and `CONFIG_KPROBES` are enabled at the same time.
    - For kernel 5.10
    
    Signed-off-by: dawfukfr <dawfukfr@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1795,6 +1795,14 @@ static int exec_binprm(struct linux_binprm *bprm)
 	return 0;
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+extern bool ksu_execveat_hook __read_mostly;
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+				 void *argv, void *envp, int *flags);
+#endif
+
 /*
  * sys_execve() executes a new program.
  */
@@ -1885,6 +1893,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 {
 	struct linux_binprm *bprm;
 	int retval;
+	
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
