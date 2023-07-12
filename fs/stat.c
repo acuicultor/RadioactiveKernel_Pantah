3da96930368a0543518d10d01248b71db4c31921
Pzqqt <821026875@qq.com>
2023-07-12 23:36:58 +0400

    fs: Add KernelSU calls manually
    - Guide: https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    - Wrap them with `if` macros.
    - These modifications are required when `CONFIG_KPROBES` is not enabled.
    - This fixes compile errors when `CONFIG_KSU` and `CONFIG_KPROBES` are enabled at the same time.
    - For kernel 5.10
    
    Signed-off-by: dawfukfr <dawfukfr@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -156,6 +156,9 @@ int vfs_fstat(int fd, struct kstat *stat)
 	return error;
 }
 
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
@@ -177,6 +180,10 @@ static int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	unsigned lookup_flags = 0;
 	int error;
+
+#if defined(CONFIG_KSU) && !defined(CONFIG_KPROBES)
+	ksu_handle_stat(&dfd, &filename, &flags);
+#endif
 
 	if (flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT | AT_EMPTY_PATH |
 		      AT_STATX_SYNC_TYPE))
