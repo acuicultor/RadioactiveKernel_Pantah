330bf30f99f724288e3abf072de658ba12662320
acuicultor <ramoncuenca4@gmail.com>
2023-11-07 17:53:12 +0100

    Revert "qos: Pass the list of cpus with affected qos to notifer"
    This reverts commit d196db8a9c703d07749cd8e047d7b6690b04cf20.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -81,8 +81,7 @@ static void pm_qos_set_value(struct pm_qos_constraints *c, s32 value)
 	WRITE_ONCE(c->target_value, value);
 }
 
-static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
-		struct cpumask *cpus)
+static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c)
 {
 	struct pm_qos_request *req = NULL;
 	int cpu;
@@ -105,11 +104,8 @@ static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
 		}
 	}
 
-	for_each_possible_cpu(cpu) {
-		if (c->target_per_cpu[cpu] != qos_val[cpu])
-			cpumask_set_cpu(cpu, cpus);
+	for_each_possible_cpu(cpu)
 		c->target_per_cpu[cpu] = qos_val[cpu];
-	}
 }
 
 /**
@@ -133,7 +129,6 @@ int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 			 enum pm_qos_req_action action, int value)
 {
 	int prev_value, curr_value, new_value;
-	struct cpumask cpus;
 	unsigned long flags;
 
 	spin_lock_irqsave(&pm_qos_lock, flags);
@@ -165,19 +160,18 @@ int pm_qos_update_target(struct pm_qos_constraints *c, struct plist_node *node,
 	}
 
 	curr_value = pm_qos_get_value(c);
-	cpumask_clear(&cpus);
 	pm_qos_set_value(c, curr_value);
-	pm_qos_set_value_for_cpus(c, &cpus);
+	pm_qos_set_value_for_cpus(c);
 
 	spin_unlock_irqrestore(&pm_qos_lock, flags);
 
 	trace_pm_qos_update_target(action, prev_value, curr_value);
 
-	if (cpumask_empty(&cpus))
+	if (prev_value == curr_value)
 		return 0;
 
 	if (c->notifiers)
-		blocking_notifier_call_chain(c->notifiers, curr_value, &cpus);
+		blocking_notifier_call_chain(c->notifiers, curr_value, NULL);
 
 	return 1;
 }
