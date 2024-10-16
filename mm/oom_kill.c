cac6f4bd27280dc349cc87a4f4389f9ae7407971
acuicultor <ramoncuenca4@gmail.com>
2024-10-16 19:14:03 +0200

    Revert "simple_lmk: Disable OOM killer when Simple LMK is enabled"
    This reverts commit d0ed7b22ce743237b4342de206ff46c4abb105ef.
    
    Signed-off-by: acuicultor <ramoncuenca4@gmail.com>
    
    # Conflicts:
    #	mm/oom_kill.c

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -1102,11 +1102,7 @@ EXPORT_SYMBOL_GPL(unregister_oom_notifier);
 bool out_of_memory(struct oom_control *oc)
 {
 	unsigned long freed = 0;
-
-	/* Return true since Simple LMK automatically kills in the background */
-	if (IS_ENABLED(CONFIG_ANDROID_SIMPLE_LMK))
-		return true;
-
+	
 	if (oom_killer_disabled)
 		return false;
 
